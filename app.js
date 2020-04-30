7d3da47d04a39e0352f037e40cfbe03927416e27
Ankit Mishra <59928958+friendlyNeighbourhoodFool@users.noreply.github.com>
2020-04-30 21:08:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,30 @@
     });
 });
 
+
+
+
+//check off specific todo by clicking
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		var todotext=$(this).val();
+		$("#g").append("<li id='li'><span id='sp'><i class='fa fa-trash'></i> </span>"+todotext+"</li>");
+		$(this).val(" ");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
+});
